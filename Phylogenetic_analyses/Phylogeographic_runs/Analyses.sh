7b2f36850aa07aed0622422bb8d64a08a2aaac9c
Simon Dellicour <simondellicour@gmail.com>
2020-10-03 13:17:44 +0200

    updating repo

--- Phylogenetic_analyses/Phylogeographic_runs/Analyses.sh
+++
@@ -1,4 +0,0 @@
-java -jar beast_1104.jar -overwrite Clade_30.xml
-java -jar beast_1104.jar -overwrite Clade_38.xml
-java -jar beast_1104.jar -overwrite Clade_129.xml
-java -jar beast_1104.jar -overwrite Clade_157.xml
